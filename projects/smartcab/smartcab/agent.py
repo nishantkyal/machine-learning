58b6e2799d6d4c70b7a111174139ba493bf0139b
Nishant Kyal <fcnk15830@ad.fc.net>
2017-08-16 18:20:14 +0530

    Reinforcement learning project - v2

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -200,7 +200,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, learning=True, alpha = 0.005)
+    agent = env.create_agent(LearningAgent, learning=True, alpha = 0.001)
     
     ##############
     # Follow the driving agent
@@ -222,7 +222,7 @@ def run():
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10, tolerance=0.001)
+    sim.run(n_test=100, tolerance=0.001)
 
 
 if __name__ == '__main__':
